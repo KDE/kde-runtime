555699553812ce565e7dbb7f1a8a97f6e0a2cb24
Waldo Bastian <bastian@kde.org>
2001-03-23 00:42:57 +0000

    drKonqi stays on top. Patch by Mark Deneen <deneen@bucknell.edu>
    svn path=/trunk/kdebase/drkonqi/; revision=88269

--- drkonqi/toplevel.cpp
+++ drkonqi/toplevel.cpp
@@ -31,6 +31,8 @@
 #include <qlabel.h>
 #include <qhbox.h>
 
+#include "netwm.h"
+
 #include <klocale.h>
 #include <kglobal.h>
 #include <kdebug.h>
@@ -60,6 +62,9 @@ Toplevel :: Toplevel(const KrashConfig *krashconf, QWidget *parent, const char *
                  ),
     m_krashconf(krashconf), bugreport(0)
 {
+  NETWinInfo inf(qt_xdisplay(), winId(), qt_xrootwin(), NET::WMState);
+  inf.setState(NET::StaysOnTop, NET::StaysOnTop);
+
   QHBox *page = addHBoxPage(i18n("General"));
   page->setSpacing(20);
 
