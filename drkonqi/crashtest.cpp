407ba8ba3267ece30805b3b57dae8a4225fc5a24
Waldo Bastian <bastian@kde.org>
2002-11-21 21:32:14 +0000

    Added new & creative ways to crash.
    svn path=/trunk/kdebase/drkonqi/; revision=190029

--- drkonqi/crashtest.cpp
+++ drkonqi/crashtest.cpp
@@ -1,31 +1,93 @@
 // Let's crash.
 #include <kapplication.h>
+#include <kaboutdata.h>
+#include <kcmdlineargs.h>
 #include <kdebug.h>
 #include <stdio.h>
+#include <assert.h>
 
-void level4()
+static KCmdLineOptions options[] =
 {
-  delete (void*)0xdead;
+   { "+crash|malloc|div0|assert", "Type of crash.", 0 },
+   { 0, 0, 0 }
+};
+
+enum CrashType { Crash, Malloc, Div0, Assert };
+
+void do_crash()
+{  
+   KCmdLineArgs *args = 0;
+   QCString type = args->arg(0);
+   printf("result = %s\n", type.data());
 }
 
-void level3()
-{
-  level4();
+void do_malloc()
+{  
+  delete (char*)0xdead;
 }
 
-void level2()
-{
-  level3();
+void do_div0()
+{  
+  volatile int a = 99;
+  volatile int b = 10;
+  volatile int c = a / ( b - 10 );
+  printf("result = %d\n", c);
 }
 
-void level1()
-{
-  level2();
+void do_assert()
+{ 
+  assert(false); 
 }
+
+void level4(int t)
+{
+  if (t == Malloc)
+    do_malloc();
+  else if (t == Div0)
+    do_div0();
+  else if (t == Assert)
+    do_assert();
+  else
+    do_crash();
+}
+
+void level3(int t)
+{
+  level4(t);
+}
+
+void level2(int t)
+{
+  level3(t);
+}
+
+void level1(int t)
+{
+  level2(t);
+}
+
 
 int main(int argc, char *argv[])
 {
-  KApplication app(argc,argv,"crashtest",false,false); 
-  level1();
+   KAboutData aboutData("crashtext", "Crash Test for DrKonqi",
+                        "1.1",
+                        "Crash Test for DrKonqi",
+                        KAboutData::License_GPL,
+                        "(c) 2000-2002 David Faure, Waldo Bastian");
+
+  KCmdLineArgs::init(argc, argv, &aboutData);
+  KCmdLineArgs::addCmdLineOptions(options);
+                                                                                                   
+  KApplication app(false,false);
+  KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
+  QCString type = args->arg(0);
+  int crashtype = Crash;
+  if (type == "malloc")
+     crashtype = Malloc;
+  else if (type == "div0")
+     crashtype = Div0;
+  else if (type == "assert")
+     crashtype = Assert;
+  level1(crashtype);
   return app.exec();
 }
