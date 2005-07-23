0ea358c735f4a92d1cad744d7e2065b409649b3b
Reinhold Kainhofer <reinhold@kainhofer.com>
2005-07-23 19:53:57 +0000

    The FSF has moved, use the new address in all license files and headers ...
    svn path=/trunk/KDE/kdepim/networkstatus/; revision=437980

---
+++ solid-networkstatus/kded/network.h
@@ -0,0 +1,67 @@
+/*
+    This file is part of kdepim.
+
+    Copyright (c) 2005 Will Stephenson <lists@stevello.free-online.co.uk>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef NETWORKSTATUS_NETWORK_H
+#define NETWORKSTATUS_NETWORK_H
+
+#include <qstringlist.h>
+#include <ksharedptr.h>
+#include "networkstatuscommon.h"
+
+struct NetworkUsageStruct
+{
+	QCString appId;
+	QString host;
+};
+
+typedef QValueList< NetworkUsageStruct > NetworkUsageList;
+
+class Network
+{
+public:
+	Network( const QString name, NetworkStatus::Properties properties );
+	Network( const Network & other );
+	NetworkStatus::EnumStatus reachabilityFor( const QString & host );
+	void registerUsage( const QCString appId, const QString host );
+	void unregisterUsage( const QCString appId, const QString host );
+	void setStatus( NetworkStatus::EnumStatus status );
+
+	void removeAllUsage();
+
+	NetworkStatus::EnumStatus status() { return m_status; }
+	QString name() { return m_name; }
+	bool internet() { return m_internet; }
+	QStringList netmasks() { return m_netmasks; }
+	QCString service() { return m_service; }
+	NetworkStatus::EnumOnDemandPolicy onDemandPolicy() { return m_onDemandPolicy; }
+	NetworkUsageList usage() { return m_usage; }
+	
+private:
+	NetworkStatus::EnumStatus m_status;
+	QString m_name;
+	bool m_internet;
+	QStringList m_netmasks;
+	QCString m_service;
+	NetworkStatus::EnumOnDemandPolicy m_onDemandPolicy;
+	NetworkUsageList m_usage;
+};
+
+#endif
---
+++ solid-networkstatus/kded/networkstatus.h
@@ -0,0 +1,80 @@
+/*
+    This file is part of kdepim.
+
+    Copyright (c) 2005 Will Stephenson <lists@stevello.free-online.co.uk>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef KDED_NETWORKSTATUS_H
+#define KDED_NETWORKSTATUS_H
+
+#include "networkstatuscommon.h"
+
+#include <kdedmodule.h>
+
+#include "clientiface.h"
+#include "network.h"
+#include "serviceiface.h"
+
+struct NetworkStatusStruct;
+
+class NetworkStatusModule : virtual public KDEDModule/*, public ClientIface, ServiceIface*/ // <-spot the multiple inheritance pb
+{
+Q_OBJECT
+K_DCOP
+public:
+	NetworkStatusModule( const QCString & obj );
+	~NetworkStatusModule();
+k_dcop:
+	// Client interface
+	QStringList networks();
+	int status( const QString & host );
+	int request( const QString & host, bool userInitiated );
+	void relinquish( const QString & host );
+	bool reportFailure( const QString & host );
+// 	QString statusAsString();
+	// Service interface
+	void setNetworkStatus( const QString & networkName, int status );
+	void registerNetwork( const QString & networkName, NetworkStatus::Properties properties );
+	void unregisterNetwork( const QString & networkName );
+	void requestShutdown( const QString & networkName );
+k_dcop_signals:
+	/**
+	 * A status change occurred for the network(s) used to connect to the given host.
+	 * @param host The host which the application has indicated it is using
+	 * @param status The new status of the network used to reach host.
+	 */
+		void statusChange( QString host, int status );
+	/**
+	 * The network would like to shut down - any clients using this host are to finish using it immediately and call 
+	 * relinquish() when done.
+	 * @param host The host, registered as in use by applications, which is about to be disconnected.
+	 */
+	void shutdownRequested( QString host );
+protected slots:
+	void registeredToDCOP( const QCString& appId );
+	void unregisteredFromDCOP( const QCString& appId );
+
+protected:
+	// QStringList networkUsers( const QString & networkName );
+	Network * networkForHost( const QString & host );
+
+private:
+	class Private;
+	Private *d;
+};
+#endif
