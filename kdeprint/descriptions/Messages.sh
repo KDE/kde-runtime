4c4d83c2c6d44d086445c09704b37d55b6bc6919
Alex Merry <kde@randomguy3.me.uk>
2007-10-22 21:00:04 +0000

    Goodbye, libkdeprint, how we loved thee.
    svn path=/trunk/KDE/kdebase/runtime/; revision=728266

--- kdeprint/descriptions/Messages.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-$XGETTEXT printers.cpp rc.cpp -o $podir/ppdtranslations4.pot
--- kdeprint/kdeprint_part/Messages.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC kdeprint_part.rc >> rc.cpp || exit 11
-$XGETTEXT *.cpp -o $podir/kdeprint_part.pot
