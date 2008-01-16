696b2bb239562c7a6db4f80bb4e150dc86e82fc1
Peter Penz <peter.penz19@gmail.com>
2008-01-16 21:21:41 +0000

    SVN_SILENT: revert commit 762337 ("coding style - fix broken indendation") - I'm sorry, this was a local problem with a tabs. vs. spaces setting...
    svn path=/trunk/KDE/kdebase/runtime/; revision=762357

--- knetattach/knetattach.cpp
+++ knetattach/knetattach.cpp
@@ -53,13 +53,13 @@ KNetAttach::KNetAttach( QWidget* parent )
     KConfigGroup recent(&crecent, "General");
     QStringList idx = recent.readEntry("Index",QStringList());
     if (idx.isEmpty()) {
-        _recent->setEnabled(false);
-        if (_recent->isChecked()) {
-            _webfolder->setChecked(true);
-        }
+	_recent->setEnabled(false);
+	if (_recent->isChecked()) {
+	    _webfolder->setChecked(true);
+	}
     } else {
-        _recent->setEnabled(true);
-        _recentConnectionName->addItems(idx);
+	_recent->setEnabled(true);
+	_recentConnectionName->addItems(idx);
     }
 }
 
@@ -68,9 +68,9 @@ void KNetAttach::setInformationText( const QString &type )
     QString text;
 
     if (type=="WebFolder") {
-        text = i18n("Enter a name for this <i>WebFolder</i> as well as a server address, port and folder path to use and press the <b>Save & Connect</b> button.");
+	text = i18n("Enter a name for this <i>WebFolder</i> as well as a server address, port and folder path to use and press the <b>Save & Connect</b> button.");
     } else if (type=="Fish") {
-        text = i18n("Enter a name for this <i>Secure shell connection</i> as well as a server address, port and folder path to use and press the <b>Save & Connect</b> button.");
+	text = i18n("Enter a name for this <i>Secure shell connection</i> as well as a server address, port and folder path to use and press the <b>Save & Connect</b> button.");
     } else if (type=="FTP") {
         text = i18n("Enter a name for this <i>File Transfer Protocol connection</i> as well as a server address and folder path to use and press the <b>Save & Connect</b> button.");
     } else if (type=="SMB") {
@@ -84,63 +84,63 @@ void KNetAttach::showPage( QWidget *page )
 {
     if (page == _folderType) {
     } else if (page == _folderParameters) {
-        _host->setFocus();
-        _connectionName->setFocus();
+	_host->setFocus();
+	_connectionName->setFocus();
 
-        if (_webfolder->isChecked()) {
-            setInformationText("WebFolder");
-            updateForProtocol("WebFolder");
-            _port->setValue(80);
-        } else if (_fish->isChecked()) {
-            setInformationText("Fish");
-            updateForProtocol("Fish");
-            _port->setValue(22);
-        } else if (_ftp->isChecked()) {
-            setInformationText("FTP");
-            updateForProtocol("FTP");
-            _port->setValue(21);
-            if (_path->text().isEmpty()) {
-                _path->setText("/");
-            }
-        } else if (_smb->isChecked()) {
-            setInformationText("SMB");
-            updateForProtocol("SMB");
-        } else { //if (_recent->isChecked()) {
-            KConfig recent( "krecentconnections", KConfig::NoGlobals );
-            if (!recent.hasGroup(_recentConnectionName->currentText())) {
-                KConfigGroup group = recent.group("General");
-                QStringList idx = group.readEntry("Index",QStringList());
-                if (idx.isEmpty()) {
-                    _recent->setEnabled(false);
-                    if (_recent->isChecked()) {
-                        _webfolder->setChecked(true);
-                    }
-                } else {
-                    _recent->setEnabled(true);
-                    _recentConnectionName->addItems(idx);
-                }
-                showPage(_folderType);
-                return;
-            }
-            KConfigGroup group = recent.group(_recentConnectionName->currentText());
-            _type = group.readEntry("Type");
-            setInformationText(_type);
-            if (!updateForProtocol(_type)) {
-                // FIXME: handle error
-            }
-            KUrl u(group.readEntry("URL"));
-            _host->setText(u.host());
-            _user->setText(u.user());
-            _path->setText(u.path());
-            if (group.hasKey("Port")) {
-                _port->setValue(group.readEntry("Port",0));
-            } else {
-                _port->setValue(u.port());
-            }
-            _connectionName->setText(_recentConnectionName->currentText());
-            _createIcon->setChecked(false);
-        }
-        updateParametersPageStatus();
+	if (_webfolder->isChecked()) {
+	    setInformationText("WebFolder");
+	    updateForProtocol("WebFolder");
+	    _port->setValue(80);
+	} else if (_fish->isChecked()) {
+	    setInformationText("Fish");
+	    updateForProtocol("Fish");
+	    _port->setValue(22);
+	} else if (_ftp->isChecked()) {
+	    setInformationText("FTP");
+	    updateForProtocol("FTP");
+	    _port->setValue(21);
+	    if (_path->text().isEmpty()) {
+		_path->setText("/");
+	    }
+	} else if (_smb->isChecked()) {
+	    setInformationText("SMB");
+	    updateForProtocol("SMB");
+	} else { //if (_recent->isChecked()) {
+	    KConfig recent( "krecentconnections", KConfig::NoGlobals );
+	    if (!recent.hasGroup(_recentConnectionName->currentText())) {
+		KConfigGroup group = recent.group("General");
+		QStringList idx = group.readEntry("Index",QStringList());
+		if (idx.isEmpty()) {
+		    _recent->setEnabled(false);
+		    if (_recent->isChecked()) {
+			_webfolder->setChecked(true);
+		    }
+		} else {
+		    _recent->setEnabled(true);
+		    _recentConnectionName->addItems(idx);
+		}
+		showPage(_folderType);
+		return;
+	    }
+	    KConfigGroup group = recent.group(_recentConnectionName->currentText());
+	    _type = group.readEntry("Type");
+	    setInformationText(_type);
+	    if (!updateForProtocol(_type)) {
+		// FIXME: handle error
+	    }
+	    KUrl u(group.readEntry("URL"));
+	    _host->setText(u.host());
+	    _user->setText(u.user());
+	    _path->setText(u.path());
+	    if (group.hasKey("Port")) {
+		_port->setValue(group.readEntry("Port",0));
+	    } else {
+		_port->setValue(u.port());
+	    }
+	    _connectionName->setText(_recentConnectionName->currentText());
+	    _createIcon->setChecked(false);
+	}
+	updateParametersPageStatus();
     }
 
     Q3Wizard::showPage(page);
@@ -150,9 +150,9 @@ void KNetAttach::showPage( QWidget *page )
 void KNetAttach::updateParametersPageStatus()
 {
     setFinishEnabled(_folderParameters,
-        !_host->text().trimmed().isEmpty() &&
-        !_path->text().trimmed().isEmpty() &&
-        !_connectionName->text().trimmed().isEmpty());
+		  !_host->text().trimmed().isEmpty() &&
+		  !_path->text().trimmed().isEmpty() &&
+		  !_connectionName->text().trimmed().isEmpty());
 }
 
 void KNetAttach::finished()
@@ -161,20 +161,20 @@ void KNetAttach::finished()
     setFinishEnabled(_folderParameters, false);
     KUrl url;
     if (_type == "WebFolder") {
-        if (_useEncryption->isChecked()) {
-            url.setProtocol("webdavs");
-        } else {
-            url.setProtocol("webdav");
-        }
-        url.setPort(_port->value());
+	if (_useEncryption->isChecked()) {
+	    url.setProtocol("webdavs");
+	} else {
+	    url.setProtocol("webdav");
+	}
+	url.setPort(_port->value());
     } else if (_type == "Fish") {
-        url.setProtocol("fish");
-        url.setPort(_port->value());
+	url.setProtocol("fish");
+	url.setPort(_port->value());
     } else if (_type == "FTP") {
-        url.setProtocol("ftp");
-        url.setPort(_port->value());
+	url.setProtocol("ftp");
+	url.setPort(_port->value());
     } else if (_type == "SMB") {
-        url.setProtocol("smb");
+	url.setProtocol("smb");
     } else { // recent
     }
 
@@ -182,17 +182,17 @@ void KNetAttach::finished()
     url.setUser(_user->text().trimmed());
     QString path = _path->text().trimmed();
     if (!path.startsWith('/')) {
-        path = QString("/") + path;
+	path = QString("/") + path;
     }
     url.setPath(path);
-    _folderParameters->setEnabled(false);
+   _folderParameters->setEnabled(false);
     bool success = doConnectionTest(url);
-    _folderParameters->setEnabled(true);
+   _folderParameters->setEnabled(true);
     if (!success) {
-        KMessageBox::sorry(this, i18n("Unable to connect to server.  Please check your settings and try again."));
-        showPage(_folderParameters);
-        setBackEnabled(_folderParameters, true);
-        return;
+	KMessageBox::sorry(this, i18n("Unable to connect to server.  Please check your settings and try again."));
+	showPage(_folderParameters);
+	setBackEnabled(_folderParameters, true);
+	return;
     }
 
     KToolInvocation::invokeBrowser(url.url());
@@ -200,46 +200,46 @@ void KNetAttach::finished()
     QString name = _connectionName->text().trimmed();
 
     if (_createIcon->isChecked()) {
-        KGlobal::dirs()->addResourceType("remote_entries", "data", "remoteview");
+	KGlobal::dirs()->addResourceType("remote_entries", "data", "remoteview");
 
-        QString path = KGlobal::dirs()->saveLocation("remote_entries");
-        path += name + ".desktop";
-        KConfig _desktopFile( path, KConfig::SimpleConfig );
-        KConfigGroup desktopFile(&_desktopFile, "Desktop Entry");
-        desktopFile.writeEntry("Icon", "package-network");
-        desktopFile.writeEntry("Name", name);
-        desktopFile.writeEntry("Type", "Link");
-        desktopFile.writeEntry("URL", url.prettyUrl());
-        desktopFile.sync();
-        org::kde::KDirNotify::emitFilesAdded( "remote:/" );
+	QString path = KGlobal::dirs()->saveLocation("remote_entries");
+	path += name + ".desktop";
+	KConfig _desktopFile( path, KConfig::SimpleConfig );
+	KConfigGroup desktopFile(&_desktopFile, "Desktop Entry");
+	desktopFile.writeEntry("Icon", "package-network");
+	desktopFile.writeEntry("Name", name);
+	desktopFile.writeEntry("Type", "Link");
+	desktopFile.writeEntry("URL", url.prettyUrl());
+	desktopFile.sync();
+	org::kde::KDirNotify::emitFilesAdded( "remote:/" );
     }
 
     if (!name.isEmpty()) {
-        KConfig _recent("krecentconnections", KConfig::NoGlobals);
-        KConfigGroup recent(&_recent, "General");
-        QStringList idx = recent.readEntry("Index",QStringList());
-        _recent.deleteGroup(name); // erase anything stale
-        if (idx.contains(name)) {
-            idx.removeAll(name);
-            idx.prepend(name);
-            recent.writeEntry("Index", idx);
-        } else {
-            QString last;
-            if (!idx.isEmpty()) {
-            last = idx.last();
-            idx.pop_back();
-            }
-            idx.prepend(name);
-            _recent.deleteGroup(last);
-            recent.writeEntry("Index", idx);
-        }
-        recent = KConfigGroup(&_recent,name);
-        recent.writeEntry("URL", url.prettyUrl());
-        if (_type == "WebFolder" || _type == "Fish" || _type == "FTP") {
-            recent.writeEntry("Port", _port->value());
-        }
-        recent.writeEntry("Type", _type);
-        recent.sync();
+	KConfig _recent("krecentconnections", KConfig::NoGlobals);
+	KConfigGroup recent(&_recent, "General");
+	QStringList idx = recent.readEntry("Index",QStringList());
+	_recent.deleteGroup(name); // erase anything stale
+	if (idx.contains(name)) {
+	    idx.removeAll(name);
+	    idx.prepend(name);
+	    recent.writeEntry("Index", idx);
+	} else {
+	    QString last;
+	    if (!idx.isEmpty()) {
+		last = idx.last();
+		idx.pop_back();
+	    }
+	    idx.prepend(name);
+	    _recent.deleteGroup(last);
+	    recent.writeEntry("Index", idx);
+	}
+       recent = KConfigGroup(&_recent,name);
+	recent.writeEntry("URL", url.prettyUrl());
+	if (_type == "WebFolder" || _type == "Fish" || _type == "FTP") {
+	    recent.writeEntry("Port", _port->value());
+	}
+	recent.writeEntry("Type", _type);
+	recent.sync();
     }
 
     QDialog::accept();
@@ -249,11 +249,11 @@ void KNetAttach::finished()
 void KNetAttach::updatePort(bool encryption)
 {
     if (_webfolder->isChecked()) {
-        if (encryption) {
-            _port->setValue(443);
-        } else {
-            _port->setValue(80);
-        }
+	if (encryption) {
+	    _port->setValue(443);
+	} else {
+	    _port->setValue(80);
+	}
     }
 }
 
@@ -262,8 +262,8 @@ bool KNetAttach::doConnectionTest(const KUrl& url)
 {
     KIO::UDSEntry entry;
     if (KIO::NetAccess::stat(url, entry, this)) {
-        // Anything to test here?
-        return true;
+	// Anything to test here?
+	return true;
     }
     return false;
 }
@@ -273,32 +273,32 @@ bool KNetAttach::updateForProtocol(const QString& protocol)
 {
     _type = protocol;
     if (protocol == "WebFolder") {
-        _useEncryption->show();
-        _portText->show();
-        _port->show();
-        _userText->show();
-        _user->show();
+	_useEncryption->show();
+	_portText->show();
+	_port->show();
+	_userText->show();
+	_user->show();
     } else if (protocol == "Fish") {
-        _useEncryption->hide();
-        _portText->show();
-        _port->show();
-        _userText->show();
-        _user->show();
+	_useEncryption->hide();
+	_portText->show();
+	_port->show();
+	_userText->show();
+	_user->show();
     } else if (protocol == "FTP") {
-        _useEncryption->hide();
-        _portText->show();
-        _port->show();
-        _userText->show();
-        _user->show();
+	_useEncryption->hide();
+	_portText->show();
+	_port->show();
+	_userText->show();
+	_user->show();
     } else if (protocol == "SMB") {
-        _useEncryption->hide();
-        _portText->hide();
-        _port->hide();
-        _userText->hide();
-        _user->hide();
+	_useEncryption->hide();
+	_portText->hide();
+	_port->hide();
+	_userText->hide();
+	_user->hide();
     } else {
-        _type = "";
-        return false;
+	_type = "";
+	return false;
     }
     return true;
 }
@@ -307,9 +307,9 @@ bool KNetAttach::updateForProtocol(const QString& protocol)
 void KNetAttach::updateFinishButtonText(bool save)
 {
     if (save) {
-        finishButton()->setText(i18n("Save && C&onnect"));
+	finishButton()->setText(i18n("Save && C&onnect"));
     } else {
-        finishButton()->setText(i18n("C&onnect"));
+	finishButton()->setText(i18n("C&onnect"));
     }
 }
 
