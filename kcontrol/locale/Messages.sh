23727258c242ff3a01649d5c76f0194210769b5b
Thomas Reitelbach <tr@erdfunkstelle.de>
2008-01-18 18:17:52 +0000

    revert 762802.
    Hm, all necessary strings have already been extracted because 
    scripty automatically extracts ui-files in the same dir where 
    Messages.sh resides.
    
    The reason for all those modules beeing untranslated must 
    be something else. I'll have to investigate it.
    
    
    svn path=/branches/KDE/4.0/kdebase/runtime/; revision=763144

--- kcontrol/locale/Messages.sh
+++ kcontrol/locale/Messages.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
 $XGETTEXT -ktranslate:1,1t -ktranslate:1c,2,2t *.cpp -o $podir/kcmlocale.pot
 $XGETTEXT TIMEZONES rc.cpp -o $podir/../kdelibs/timezones4.pot
