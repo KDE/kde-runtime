2871480023e9d093d7b14ffb608cedf11e8ca960
Dmitry Suzdalev <dimsuz@gmail.com>
2008-05-04 16:46:46 +0000

    Add myself to the copyright headers, so people would know who to contact if something will break or whatever else they would like to tell :)
    svn path=/trunk/KDE/kdebase/runtime/; revision=803971

--- knotify/notifybypopup.cpp
+++ knotify/notifybypopup.cpp
@@ -1,6 +1,6 @@
 /*
    Copyright (C) 2005-2006 by Olivier Goffart <ogoffart at kde.org>
-
+   Copyright (C) 2008 by Dmitry Suzdalev <dimsuz@gmail.com>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
