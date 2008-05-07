72c7589ddfea9b0a8ee6366c06a6af4cd61ee69a
Sebastian Trueg <sebastian@trueg.de>
2008-05-07 12:12:52 +0000

    Copyright statements fixed
    svn path=/trunk/KDE/kdebase/runtime/; revision=805035

--- nepomuk/services/storage/cluceneanalyzer.cpp
+++ nepomuk/services/storage/cluceneanalyzer.cpp
@@ -1,15 +1,26 @@
 /*
  * Modified version of StandardAnalyzer.cpp for Nepomuk mostly to optimize for filename indexing
- *
  * Copyright (C) 2008 Sebastian Trueg <trueg@kde.org>
+ *
+ * Based on StandardAnalyzer.cpp from the CLucene package.
+ * Copyright (C) 2003-2006 Ben van Klinken and the CLucene Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  */
 
-/*------------------------------------------------------------------------------
-* Copyright (C) 2003-2006 Ben van Klinken and the CLucene Team
-*
-* Distributable under the terms of either the Apache License (Version 2.0) or
-* the GNU Lesser General Public License, as specified in the COPYING file.
-------------------------------------------------------------------------------*/
 #include "cluceneanalyzer.h"
 #include "clucenetokenizer.h"
 
