9bcd4c71db3d3d68b70a1a99ac576b04df3d3122
Matthias Kretz <kretz@kde.org>
2008-09-01 16:23:10 +0000

    whatever magic pkg_check_modules does to the variables... I better use my own variables, then it works to find libpulse after it wasn't installed before
    svn path=/trunk/KDE/kdebase/runtime/; revision=855868

--- cmake/modules/FindPulseAudio.cmake
+++ cmake/modules/FindPulseAudio.cmake
@@ -3,7 +3,7 @@
 # Once done this will define:
 #
 #  PULSEAUDIO_FOUND - system has the PulseAudio library
-#  PULSEAUDIO_INCLUDEDIR - the PulseAudio include directory
+#  PULSEAUDIO_INCLUDE_DIR - the PulseAudio include directory
 #  PULSEAUDIO_LIBRARY - the libraries needed to use PulseAudio
 #
 # Copyright (c) 2008, Matthias Kretz, <kretz@kde.org>
@@ -11,36 +11,35 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if (PULSEAUDIO_INCLUDEDIR AND PULSEAUDIO_LIBRARY)
+if (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY)
    # Already in cache, be silent
    set(PULSEAUDIO_FIND_QUIETLY TRUE)
-endif (PULSEAUDIO_INCLUDEDIR AND PULSEAUDIO_LIBRARY)
+endif (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY)
 
 if (NOT WIN32)
-   # use pkg-config to get the directories and then use these values
-   # in the FIND_PATH() and FIND_LIBRARY() calls
    include(FindPkgConfig)
    pkg_check_modules(PULSEAUDIO libpulse)
    if(PULSEAUDIO_FOUND)
-      set(PULSEAUDIO_LIBRARY ${PULSEAUDIO_LIBRARIES})
+      set(PULSEAUDIO_LIBRARY ${PULSEAUDIO_LIBRARIES} CACHE FILEPATH "Path to the PulseAudio library")
+      set(PULSEAUDIO_INCLUDE_DIR ${PULSEAUDIO_INCLUDEDIR} CACHE PATH "Path to the PulseAudio includes")
       #  PULSEAUDIO_DEFINITIONS - Compiler switches required for using PulseAudio
       #  set(PULSEAUDIO_DEFINITIONS ${PULSEAUDIO_CFLAGS})
    endif(PULSEAUDIO_FOUND)
 endif (NOT WIN32)
 
-if (NOT PULSEAUDIO_INCLUDEDIR)
-   FIND_PATH(PULSEAUDIO_INCLUDEDIR pulse/pulseaudio.h)
-endif (NOT PULSEAUDIO_INCLUDEDIR)
+if (NOT PULSEAUDIO_INCLUDE_DIR)
+   FIND_PATH(PULSEAUDIO_INCLUDE_DIR pulse/pulseaudio.h)
+endif (NOT PULSEAUDIO_INCLUDE_DIR)
 
 if (NOT PULSEAUDIO_LIBRARY)
    FIND_LIBRARY(PULSEAUDIO_LIBRARY NAMES pulse)
 endif (NOT PULSEAUDIO_LIBRARY)
 
-if (PULSEAUDIO_INCLUDEDIR AND PULSEAUDIO_LIBRARY)
+if (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY)
    set(PULSEAUDIO_FOUND TRUE)
-else (PULSEAUDIO_INCLUDEDIR AND PULSEAUDIO_LIBRARY)
+else (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY)
    set(PULSEAUDIO_FOUND FALSE)
-endif (PULSEAUDIO_INCLUDEDIR AND PULSEAUDIO_LIBRARY)
+endif (PULSEAUDIO_INCLUDE_DIR AND PULSEAUDIO_LIBRARY)
 
 if (PULSEAUDIO_FOUND)
    if (NOT PULSEAUDIO_FIND_QUIETLY)
@@ -50,4 +49,4 @@ else (PULSEAUDIO_FOUND)
    message(STATUS "Could NOT find PulseAudio")
 endif (PULSEAUDIO_FOUND)
 
-mark_as_advanced(PULSEAUDIO_INCLUDEDIR PULSEAUDIO_LIBRARY)
+#mark_as_advanced(PULSEAUDIO_INCLUDE_DIR PULSEAUDIO_LIBRARY)
