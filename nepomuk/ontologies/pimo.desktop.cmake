69ce3bfa36adc63bb61a2e21b8fc9628cc36da88
Sebastian Trueg <sebastian@trueg.de>
2008-10-06 14:44:03 +0000

    kdelibs now have pimo support. Thus, we also need the ontology
    svn path=/trunk/KDE/kdebase/runtime/; revision=868535

---
+++ nepomuk/ontologies/pimo.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Version=1.0
+Name=PIMO
+Comment=The Personal Information Model Ontology can be used to express personal information of individuals.
+URL=http://www.semanticdesktop.org/ontologies/2007/11/01/pimo#
+Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/pimo.trig
+MimeType=application/x-trig
+Type=Data
