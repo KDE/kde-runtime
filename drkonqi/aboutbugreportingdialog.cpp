bfc3417abecf93bfae428e950f6e46d95917646b
Darío Andrés Rodríguez <andresbajotierra@gmail.com>
2009-02-19 01:20:03 +0000

    Added licenses
    svn path=/trunk/playground/base/drkonqi/; revision=928154

--- drkonqi/aboutbugreportingdialog.cpp
+++ drkonqi/aboutbugreportingdialog.cpp
@@ -1,3 +1,22 @@
+/*******************************************************************
+* aboutbugreportingdialog.cpp
+* Copyright 2009    Dario Andres Rodriguez <andresbajotierra@gmail.com>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of 
+* the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* 
+******************************************************************/
+
 #include "aboutbugreportingdialog.h"
 
 #include <klocale.h>
@@ -21,6 +40,7 @@ AboutBugReportingDialog::AboutBugReportingDialog(QWidget * parent):
     "In order to generate an useful bug report we need to fetch some information about the crash and your system.<br /><br />"
     "We also need you to specify some information about the crash.<br /><br />"
     "<strong>Notice:</strong> You are not forced to file a bug report if you don't want to.<br />If you have never seen this dialog before and you don't know what to do you can close it"
+    "<br /><br />ToDo - Bug Reporting Assistant Steps Guide"
     ;
         
     textBrowser->setText( text );
