e719cdf267a7e57f16d91207e5f95ad443585569
Darío Andrés Rodríguez <andresbajotierra@gmail.com>
2009-03-04 23:49:56 +0000

    Small fixes , updated ToDo
    svn path=/branches/work/drkonqi-newdialog/; revision=935397

--- drkonqi/drkonqiassistantpages_bugzilla.cpp
+++ drkonqi/drkonqiassistantpages_bugzilla.cpp
@@ -17,6 +17,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 * 
 ******************************************************************/
+
 #include "drkonqiassistantpages_bugzilla.h"
 
 #include <QtCore/QDate>
