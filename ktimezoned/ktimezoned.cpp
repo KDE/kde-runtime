4d00ebac6f8d78a3920a5f436a4eaa8b0e440c4c
Till Adam <adam@kde.org>
2009-04-01 19:08:16 +0000

    Fix the build.
    svn path=/branches/kdepim/enterprise4/kdebase-4.1-branch/runtime/; revision=947948

--- ktimezoned/ktimezoned.cpp
+++ ktimezoned/ktimezoned.cpp
@@ -58,8 +58,8 @@ const char ZONE_TAB_CACHE[] = "ZonetabCache";  // type of cached simulated zone.
 const char LOCAL_ZONE[]     = "LocalZone";     // name of local time zone
 
 
-KTimeZoned::KTimeZoned(QObject* parent, const QList<QVariant>&)
-  : KTimeZoneDBase(parent),
+KTimeZoned::KTimeZoned(QObject* parent, const QList<QVariant>&, l)
+  : KTimeZoneDBase(parent, l),
     mSource(0),
     mZonetabWatch(0),
     mDirWatch(0)
