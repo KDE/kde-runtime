e22e90aa1bd1e4ed0de440be2dde3b7eceb8bba7
David Faure <faure@kde.org>
2009-08-17 17:28:25 +0000

    Do the version check only if libssh was actually found. Fixes this cmake error: CMake Error at runtime/cmake/modules/FindLibSSH.cmake:58 (file):   file STRINGS file   "/d/kde/src/t/kdebase/runtime/kioslave/LIBSSH_INCLUDE_DIR-NOTFOUND/libssh/libssh.h"   cannot be read.
    svn path=/trunk/KDE/kdebase/runtime/; revision=1012451

--- cmake/modules/FindLibSSH.cmake
+++ cmake/modules/FindLibSSH.cmake
@@ -52,24 +52,24 @@ else (LIBSSH_LIBRARIES AND LIBSSH_INCLUDE_DIRS)
       ${LIBSSH_LIBRARIES}
       ${SSH_LIBRARY}
     )
-  endif (SSH_FOUND)
 
-  if (LibSSH_FIND_VERSION)
-    file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MAJOR
+    if (LibSSH_FIND_VERSION)
+      file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MAJOR
         REGEX "#define[ ]+LIBSSH_VERSION_MAJOR[ ]+[0-9]+")
-    string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MAJOR ${LIBSSH_VERSION_MAJOR})
-    file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MINOR
+      string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MAJOR ${LIBSSH_VERSION_MAJOR})
+      file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MINOR
         REGEX "#define[ ]+LIBSSH_VERSION_MINOR[ ]+[0-9]+")
-    string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MINOR ${LIBSSH_VERSION_MINOR})
-    file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_PATCH
+      string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MINOR ${LIBSSH_VERSION_MINOR})
+      file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_PATCH
         REGEX "#define[ ]+LIBSSH_VERSION_MICRO[ ]+[0-9]+")
-    string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_PATCH ${LIBSSH_VERSION_PATCH})
+      string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_PATCH ${LIBSSH_VERSION_PATCH})
 
-    set(LibSSH_VERSION ${LIBSSH_VERSION_MAJOR}.${LIBSSH_VERSION_MINOR}.${LIBSSH_VERSION_PATCH})
+      set(LibSSH_VERSION ${LIBSSH_VERSION_MAJOR}.${LIBSSH_VERSION_MINOR}.${LIBSSH_VERSION_PATCH})
 
-    include(FindPackageVersionCheck)
-    find_package_version_check(LibSSH DEFAULT_MSG)
-  endif (LibSSH_FIND_VERSION)
+      include(FindPackageVersionCheck)
+      find_package_version_check(LibSSH DEFAULT_MSG)
+    endif (LibSSH_FIND_VERSION)
+  endif (SSH_FOUND)
 
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(LibSSH DEFAULT_MSG LIBSSH_LIBRARIES LIBSSH_INCLUDE_DIRS)
