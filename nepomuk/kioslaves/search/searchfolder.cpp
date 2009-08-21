1c6f83c7d1827c8e8a710bf8cc6603b1979e4465
Sebastian Trueg <sebastian@trueg.de>
2009-08-21 17:05:02 +0000

    fixed regression from last fix
    svn path=/trunk/KDE/kdebase/runtime/; revision=1014150

--- nepomuk/kioslaves/search/searchfolder.cpp
+++ nepomuk/kioslaves/search/searchfolder.cpp
@@ -147,7 +147,8 @@ void Nepomuk::SearchFolder::list()
     }
 
     // list all results
-    statResults();
+    if ( !m_initialListingFinished )
+        statResults();
 
     kDebug() << "listing done";
 
