830c2790fa23c74a9f9fa7a0389fbcb9b7980561
Stephan Binner <binner@kde.org>
2009-09-08 14:40:19 +0000

    backport spell fix
    svn path=/branches/KDE/4.3/kdebase/runtime/; revision=1021212

--- nepomuk/ontologies/ncal.desktop.cmake
+++ nepomuk/ontologies/ncal.desktop.cmake
@@ -1,6 +1,6 @@
 [Desktop Entry]
 Version=1.0
-Name=Nepomuk Calender Ontology
+Name=Nepomuk Calendar Ontology
 Comment=The Nepomuk Calendaring Ontology intends to provide vocabulary for describing calendaring data (events, tasks, journal entries).
 URL=http://www.semanticdesktop.org/ontologies/2007/04/02/ncal#
 Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/ncal.trig
