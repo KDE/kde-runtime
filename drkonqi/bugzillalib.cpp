339be671c5a4e584ce1775d2bf4b48f381586b5d
Darío Andrés Rodríguez <andresbajotierra@gmail.com>
2009-11-15 23:18:51 +0000

    - Improve the bugzilla query for duplicates to get more accurate results
    svn path=/branches/KDE/4.3/kdebase/runtime/; revision=1049824

--- drkonqi/bugzillalib.cpp
+++ drkonqi/bugzillalib.cpp
@@ -41,7 +41,7 @@ static const char columns[] = "bug_severity,priority,bug_status,product,short_de
 static const char loginUrl[] = "index.cgi";
 
 static const char searchUrl[] = "buglist.cgi?query_format=advanced&short_desc_type=anywordssubstr"
-                            "&short_desc=%1&product=kde&product=%2&long_desc_type=anywordssubstr&"
+                            "&short_desc=%1&product=kde&product=%2&long_desc_type=allwordssubstr&"
                             "long_desc=%3&bug_file_loc_type=allwordssubstr&bug_file_loc=&keywords_"
                             "type=allwords&keywords=&emailtype1=substring&email1=&emailtype2=substr"
                             "ing&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=%4&chfieldto="
