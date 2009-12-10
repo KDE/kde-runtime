6a3ce5b8f60db3b7a472a9114a498980f408f34c
Sebastian Trueg <sebastian@trueg.de>
2009-12-10 10:47:17 +0000

    The playground has been converted and so have all other packages. Time to remove the ontologies from kdebase and completely rely in shared-desktop-ontologies.
    svn path=/trunk/KDE/kdebase/runtime/; revision=1060954

--- nepomuk/ontologies/ncal.desktop.cmake
+++
@@ -1,8 +0,0 @@
-[Desktop Entry]
-Version=1.0
-Name=Nepomuk Calendar Ontology
-Comment=The Nepomuk Calendaring Ontology intends to provide vocabulary for describing calendaring data (events, tasks, journal entries).
-URL=http://www.semanticdesktop.org/ontologies/2007/04/02/ncal#
-Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/ncal.trig
-MimeType=application/x-trig
-Type=Data
--- nepomuk/ontologies/nexif.desktop.cmake
+++
@@ -1,8 +0,0 @@
-[Desktop Entry]
-Version=1.0
-Name=Nepomuk EXIF Ontology
-Comment=The Nepomuk EXIF Ontology describes basic image metadata used in digital cameras and in image management software.
-URL=http://www.semanticdesktop.org/ontologies/2007/05/10/nexif#
-Path=${DATA_INSTALL_DIR}/nepomuk/ontologies/nexif.trig
-MimeType=application/x-trig
-Type=Data
