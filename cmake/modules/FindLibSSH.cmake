1369a072c6c45d2c148dec83eb0dae54f08d7d6c
Andreas Schneider <asn@cynapses.org>
2010-03-15 16:18:24 +0000

    Fixed the libssh find module.
    svn path=/trunk/KDE/kdebase/runtime/; revision=1103667

--- cmake/modules/FindLibSSH.cmake
+++ cmake/modules/FindLibSSH.cmake
@@ -29,7 +29,7 @@ else (LIBSSH_LIBRARIES AND LIBSSH_INCLUDE_DIRS)
       ${CMAKE_INCLUDE_PATH}
       ${CMAKE_INSTALL_PREFIX}/include
   )
-  
+
   find_library(SSH_LIBRARY
     NAMES
       ssh
@@ -44,14 +44,14 @@ else (LIBSSH_LIBRARIES AND LIBSSH_INCLUDE_DIRS)
   )
 
   if (LIBSSH_INCLUDE_DIR AND SSH_LIBRARY)
-    set(SSH_FOUND TRUE)
+    set(LIBSSH_FOUND TRUE)
   endif (LIBSSH_INCLUDE_DIR AND SSH_LIBRARY)
 
   set(LIBSSH_INCLUDE_DIRS
     ${LIBSSH_INCLUDE_DIR}
   )
 
-  if (SSH_FOUND)
+  if (LIBSSH_FOUND)
     set(LIBSSH_LIBRARIES
       ${LIBSSH_LIBRARIES}
       ${SSH_LIBRARY}
@@ -60,26 +60,25 @@ else (LIBSSH_LIBRARIES AND LIBSSH_INCLUDE_DIRS)
     if (LibSSH_FIND_VERSION)
       file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MAJOR
         REGEX "#define[ ]+LIBSSH_VERSION_MAJOR[ ]+[0-9]+")
+
       # Older versions of libssh like libssh-0.2 have LIBSSH_VERSION but not LIBSSH_VERSION_MAJOR
       if (LIBSSH_VERSION_MAJOR)
         string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MAJOR ${LIBSSH_VERSION_MAJOR})
-	file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MINOR
+        file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_MINOR
           REGEX "#define[ ]+LIBSSH_VERSION_MINOR[ ]+[0-9]+")
-	string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MINOR ${LIBSSH_VERSION_MINOR})
-	file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_PATCH
+        string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_MINOR ${LIBSSH_VERSION_MINOR})
+        file(STRINGS ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h LIBSSH_VERSION_PATCH
           REGEX "#define[ ]+LIBSSH_VERSION_MICRO[ ]+[0-9]+")
-	string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_PATCH ${LIBSSH_VERSION_PATCH})
+        string(REGEX MATCH "[0-9]+" LIBSSH_VERSION_PATCH ${LIBSSH_VERSION_PATCH})
 
-	set(LibSSH_VERSION ${LIBSSH_VERSION_MAJOR}.${LIBSSH_VERSION_MINOR}.${LIBSSH_VERSION_PATCH})
+        set(LIBSSH_VERSION ${LIBSSH_VERSION_MAJOR}.${LIBSSH_VERSION_MINOR}.${LIBSSH_VERSION_PATCH})
 
-	include(FindPackageVersionCheck)
-	find_package_version_check(LibSSH DEFAULT_MSG)
       else (LIBSSH_VERSION_MAJOR)
         message(STATUS "LIBSSH_VERSION_MAJOR not found in ${LIBSSH_INCLUDE_DIR}/libssh/libssh.h, assuming libssh is too old")
         set(LIBSSH_FOUND FALSE)
       endif (LIBSSH_VERSION_MAJOR)
     endif (LibSSH_FIND_VERSION)
-  endif (SSH_FOUND)
+  endif (LIBSSH_FOUND)
 
   # If the version is too old, but libs and includes are set,
   # find_package_handle_standard_args will set LIBSSH_FOUND to TRUE again,
