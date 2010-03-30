13f3ea5bd3b1b5a03a6f85ba274b0572ea876469
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-03-30 16:15:06 +0000

    backport: r1109123 Properly account for the fact that Hover takes precedence over Focus in PushButtons to trigger animations. This fixes a couple of glitches/flicker when pressing on non-editable comboboxes, as well as pushbuttons.
    svn path=/branches/KDE/4.4/kdebase/runtime/; revision=1109126

--- kstyles/oxygen/oxygen.cpp
+++ kstyles/oxygen/oxygen.cpp
@@ -794,7 +794,7 @@ bool OxygenStyle::drawPushButtonPrimitive(
 
             // update animation state
             animations().widgetStateEngine().updateState( widget, Oxygen::AnimationHover, mouseOver );
-            animations().widgetStateEngine().updateState( widget, Oxygen::AnimationFocus, hasFocus );
+            animations().widgetStateEngine().updateState( widget, Oxygen::AnimationFocus, hasFocus && !mouseOver );
 
             // store animation state
             bool hoverAnimated( animations().widgetStateEngine().isAnimated( widget, Oxygen::AnimationHover ) );
@@ -2536,7 +2536,7 @@ bool OxygenStyle::drawComboBoxPrimitive(
     QRect editField = subControlRect(CC_ComboBox, qstyleoption_cast<const QStyleOptionComplex*>(opt), SC_ComboBoxEditField, widget);
 
     animations().lineEditEngine().updateState( widget, Oxygen::AnimationHover, mouseOver );
-    animations().lineEditEngine().updateState( widget, Oxygen::AnimationFocus, hasFocus );
+    animations().lineEditEngine().updateState( widget, Oxygen::AnimationFocus, hasFocus && !mouseOver );
 
     switch (primitive)
     {
