677a9c6a5686cd488c954b4689507e708b70929f
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2010-04-10 19:13:13 +0000

    backport r1113401: removed double-initialization of labelEngine duration
    svn path=/branches/KDE/4.4/kdebase/runtime/; revision=1113402

--- kstyles/oxygen/transitions/oxygentransitions.cpp
+++ kstyles/oxygen/transitions/oxygentransitions.cpp
@@ -58,8 +58,6 @@ namespace Oxygen
         labelEngine().setDuration( OxygenStyleConfigData::labelTransitionsDuration() );
         lineEditEngine().setDuration( OxygenStyleConfigData::lineEditTransitionsDuration() );
         stackedWidgetEngine().setDuration( OxygenStyleConfigData::stackedWidgetTransitionsDuration() );
-        labelEngine().setDuration( OxygenStyleConfigData::labelTransitionsDuration() );
-
 
     }
 
