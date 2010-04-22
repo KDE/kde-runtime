3a1b3996ff770b9a9352ef15b8094ef9c7dbb860
Sebastian Trueg <sebastian@trueg.de>
2010-04-22 09:53:12 +0000

    We do not need session management in nepomuk services
    svn path=/trunk/KDE/kdebase/runtime/; revision=1117468

--- nepomuk/servicestub/main.cpp
+++ nepomuk/servicestub/main.cpp
@@ -98,6 +98,7 @@ int main( int argc, char** argv )
 
     aboutData.setAppName( serviceName.toLocal8Bit() );
     KApplication app( true /* The strigi service actually needs a GUI at the moment */ );
+    app.disableSessionManagement();
     installSignalHandler();
     QApplication::setQuitOnLastWindowClosed( false );
 
