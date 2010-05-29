0ca2702c3da237d93fd5cd0cff946d4ae3f52a68
Markus Meik Slopianka <markus.s@kdemail.net>
2010-05-29 13:07:35 +0000

    Reflect KDE rebranding CCMAIL: kde-i18n-doc@kde.org
    svn path=/trunk/KDE/kdebase/runtime/; revision=1132037

--- kcontrol/locale/kcmlocale.cpp
+++ kcontrol/locale/kcmlocale.cpp
@@ -93,7 +93,7 @@ KLocaleConfig::KLocaleConfig(KControlLocale *locale, QWidget *parent)
     m_downButton->setIcon(KIcon("arrow-down"));
 
     languageAdd->loadAllLanguages();
-    
+
     KConfigGroup configGroup = KGlobal::config()->group("Locale");
     m_languageList = configGroup.readEntry("Language").split(':',QString::SkipEmptyParts);
 }
@@ -225,7 +225,7 @@ void KLocaleConfig::slotTranslate()
   languageAdd->setText( ki18n("Add Language").toString(m_locale) );
 
   m_selectedCountryLabel->setToolTip( ki18n
-        ( "This is where you live. KDE will use the defaults for "
+        ( "This is where you live. KDE workspace will use the defaults for "
           "this country or region.").toString(m_locale) );
   languageAdd->setToolTip( ki18n
         ( "This will add a language to the list. If the language is already "
@@ -249,11 +249,11 @@ void KLocaleConfig::slotTranslate()
   m_selectedCountryLabel->setWhatsThis( str );
 
   str = ki18n
-    ( "<p>Here you can choose the languages that will be used by KDE. If the "
+    ( "<p>Here you can choose the languages that will be used by the KDE workspace. If the "
       "first language in the list is not available, the second will be used, "
       "etc. If only US English is available, no translations "
       "have been installed. You can get translation packages for many "
-      "languages from the place you got KDE from.</p><p>"
+      "languages from the place you got this KDE workspace from.</p><p>"
       "Note that some applications may not be translated to your languages; "
       "in this case, they will automatically fall back to US English.</p>" ).toString(m_locale);
   m_languagesLabel->setWhatsThis( str );
