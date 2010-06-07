2c0da0cf929087b1461c263d705682713a36a02e
Christoph Feck <christoph@maxiom.de>
2010-06-07 16:37:12 +0000

    Fix browser service not reset to KIO for empty exec line (backport r1135569)
    CCBUG: 240677
    FIXED-IN: 4.4.5
    
    svn path=/branches/KDE/4.4/kdebase/runtime/; revision=1135572

--- kcontrol/componentchooser/componentchooserbrowser.cpp
+++ kcontrol/componentchooser/componentchooserbrowser.cpp
@@ -86,7 +86,7 @@ void CfgBrowser::save(KConfig *)
         exec = lineExec->text();
         if (m_browserService && (exec == m_browserExec))
             exec = m_browserService->storageId(); // Use service
-        else
+        else if (!exec.isEmpty())
             exec = '!' + exec; // Literal command
     }
     config.writePathEntry( QLatin1String("BrowserApplication"), exec); // KConfig::Normal|KConfig::Global
