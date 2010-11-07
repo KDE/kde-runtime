04768d368be79783ed1aac3f9d1b0a5db03a73f1
David Jarvie <djarvie@kde.org>
2010-11-07 18:38:30 +0000

    Update copyright email
    svn path=/trunk/KDE/kdebase/runtime/; revision=1193965

--- ktimezoned/ktimezoned.h
+++ ktimezoned/ktimezoned.h
@@ -1,6 +1,6 @@
 /*
    This file is part of the KDE libraries
-   Copyright (c) 2007,2009 David Jarvie <software@astrojar.org.uk>
+   Copyright (c) 2007,2009 David Jarvie <djarvie@kde.org>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
