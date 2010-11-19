81b4cf4a4be4d674ed260558d8ced6e3ebceb2ea
Anne-Marie Mahfouf <annma@kde.org>
2010-11-19 14:16:49 +0000

    QFileInfo needs a path
    svn path=/trunk/KDE/kdebase/runtime/; revision=1198736

--- nepomuk/kcm/nepomukserverkcm.cpp
+++ nepomuk/kcm/nepomukserverkcm.cpp
@@ -296,9 +296,10 @@ void Nepomuk::ServerConfigModule::load()
         text += QLatin1String(" (");
         text += i18nc("@info %1 is the creation date of a backup formatted vi KLocale::formatDateTime",
                       "Oldest: %1",
-                      KGlobal::locale()->formatDateTime(QFileInfo(backupFiles.last()).created(), KLocale::FancyShortDate) );
+                      KGlobal::locale()->formatDateTime(QFileInfo(backupUrl+QLatin1String("/")+backupFiles.last()).created(), KLocale::FancyShortDate));
         text += QLatin1String(")");
     }
+    
     m_labelBackupStats->setText( text );
 
     // 6. update state
