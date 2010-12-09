1c974e884882dcadb09b9f2983ce22853171de6d
Pino Toscano <pino@kde.org>
2010-12-09 23:28:14 +0000

    main() returns an int, not a boolean
    svn path=/trunk/KDE/kdebase/runtime/; revision=1204998

--- kfile/fileprops.cpp
+++ kfile/fileprops.cpp
@@ -279,7 +279,7 @@ int main( int argc, char **argv )
 
     if ( args->isSet( "dialog" ) ) {
         showPropertiesDialog( args );
-        return true;
+        return 0;
     }
 
     QString mimeType;
