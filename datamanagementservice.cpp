7d1a8271e943fed255a2a6d5fb5cbf68f7ae0f72
Sebastian Trueg <trueg@kde.org>
2011-01-21 16:00:52 +0100

    Added data management service stubs.

---
+++ datamanagementservice.cpp
@@ -0,0 +1,99 @@
+/*
+   This file is part of the Nepomuk KDE project.
+   Copyright (C) 2010-2011 Sebastian Trueg <trueg@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) version 3, or any
+   later version accepted by the membership of KDE e.V. (or its
+   successor approved by the membership of KDE e.V.), which shall
+   act as a proxy defined in Section 6 of version 3 of the license.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "datamanagementservice.h"
+
+class DataManagementService::Private
+{
+public:
+
+};
+
+
+DataManagementService::DataManagementService(QObject *parent)
+    : QObject(parent),
+      d(new Private())
+{
+}
+
+
+DataManagementService::~DataManagementService()
+{
+    delete d;
+}
+
+void DataManagementService::addProperty(const QList<QUrl> &resources, const QUrl &property, const QVariantList &values, const QString &app)
+{
+    // 1. check cardinality (via property cache/tree)
+    // 2. if cardinality 1 (we only have 1 and none):
+    //    2.1. make sure values.count() == 1 - otherwise fail
+    //    2.2. make sure no value exists - otherwise fail
+    // 3. create a new graph with all the necessary data and the app
+    // 4. check if the app exists, if not create it in the new graph
+    // 5. add the new triples to the new graph
+    // 6. update resources' mtime
+
+}
+
+void DataManagementService::setProperty(const QList<QUrl> &resources, const QUrl &property, const QVariantList &values, const QString &app)
+{
+    // 1. check cardinality (via property cache/tree)
+    // 2. if cardinality 1 (we only have 1 and none):
+    //    2.1. make sure values.count() == 1 - otherwise fail
+    // 3. get existing values
+    // 4. make the diffs between existing and new ones
+    // 5. remove the ones that are not valid anymore
+    // 6. create a new graph with all the necessary data and the app
+    // 7. check if the app exists, if not create it in the new graph
+    // 8. add the new triples to the new graph
+    // 9. update resources' mtime
+}
+
+void DataManagementService::removeProperty(const QList<QUrl> &resources, const QUrl &property, const QVariantList &values, const QString &app)
+{
+    // 1. remove the triples
+    // 2. remove trailing graphs
+    // 3. update resource mtime
+}
+
+void DataManagementService::removeProperties(const QList<QUrl> &resources, const QList<QUrl> &properties, const QString &app)
+{
+    // 1. remove the triples
+    // 2. remove trailing graphs
+    // 3. update resource mtime
+}
+
+QUrl DataManagementService::createResource(const QList<QUrl> &types, const QString &label, const QString &description, const QString &app)
+{
+    // 1. create an new graph
+    // 2. check if the app exists, if not create it in the new graph
+    // 3. create the new resource in the new graph
+    // 4. return the resource's URI
+}
+
+void DataManagementService::removeResources(const QList<QUrl> &resources, const QString &app, bool force)
+{
+    Q_UNUSED(app);
+    // 1. get all sub-resources and check if they are used by some other resource (not in the list of resources to remove)
+    //    for the latter one can use a bif:exists and a !filter(?s in <s>, <s>, ...) - based on the value of force
+    // 2. remove the resources and the sub-resources
+    // 3. drop trailing graphs (could be optimized by enumerating the graphs up front)
+}
---
+++ datamanagementservice.h
@@ -0,0 +1,170 @@
+/*
+   This file is part of the Nepomuk KDE project.
+   Copyright (C) 2011 Sebastian Trueg <trueg@kde.org>
+             (C) 2011 Vishesh Handa <handa.vish@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) version 3, or any
+   later version accepted by the membership of KDE e.V. (or its
+   successor approved by the membership of KDE e.V.), which shall
+   act as a proxy defined in Section 6 of version 3 of the license.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DATAMANAGEMENTSERVICE_H
+#define DATAMANAGEMENTSERVICE_H
+
+#include <QtCore/QObject>
+#include <QtCore/QList>
+#include <QtCore/QUrl>
+#include <QtCore/QVariant>
+
+
+/**
+ *
+ * \author Sebastian Trueg <trueg@kde.org>, Vishesh Handa <handa.vish@gmail.com>
+ */
+class DataManagementService : public QObject
+{
+    Q_OBJECT
+
+public:
+    DataManagementService(QObject *parent = 0);
+    ~DataManagementService();
+
+public Q_SLOTS:
+    /**
+     * \name Basic API
+     */
+    //@{
+    /**
+     * Add \p property with \p values to each resource
+     * from \p resources. Existing values will not be touched.
+     * If a cardinality is breached an error will be thrown.
+     */
+    void addProperty(const QList<QUrl>& resources,
+                     const QUrl& property,
+                     const QVariantList& values,
+                     const QString& app);
+
+    /**
+     * Set, ie. overwrite properties. Set \p property with
+     * \p values for each resource from \p resources. Existing
+     * values will be replaced.
+     */
+    void setProperty(const QList<QUrl>& resources,
+                     const QUrl& property,
+                     const QVariantList& values,
+                     const QString& app);
+
+    /**
+     * Remove the property \p property with \p values from each
+     * resource in \p resources.
+     */
+    void removeProperty(const QList<QUrl>& resources,
+                        const QUrl& property,
+                        const QVariantList& values,
+                        const QString& app);
+
+    /**
+     * Remove all statements involving any proerty from \p properties from
+     * all resources in \p resources.
+     */
+    void removeProperties(const QList<QUrl>& resources,
+                          const QList<QUrl>& properties,
+                          const QString& app);
+
+    /**
+     * Create a new resource with several \p types.
+     */
+    QUrl createResource(const QList<QUrl>& types,
+                        const QString& label,
+                        const QString& description,
+                        const QString& app);
+
+    /**
+     * Remove resources from the database.
+     * \param resources The URIs of the resources to be removed.
+     * \param app The calling application.
+     * \param force Force deletion of the resource and all sub-resources.
+     * If false sub-resources will be kept if they are still referenced by
+     * other resources.
+     */
+    void removeResources(const QList<QUrl>& resources,
+                         const QString& app,
+                         bool force);
+    //@}
+
+    /**
+     * \name Advanced API
+     */
+    //@{
+    /**
+     * Remove all information about resources from the database which
+     * have been created by a specific application.
+     * \param resources The URIs of the resources to be removed.
+     * \param app The application for which data should be removed.
+     * \param force Force deletion of the resource and all sub-resources.
+     * If false sub-resources will be kept if they are still referenced by
+     * other resources.
+     */
+    void removeDataByApplication(const QList<QUrl>& resources,
+                                 const QString& app,
+                                 bool force);
+
+    /**
+     * Remove all information from the database which
+     * has been created by a specific application.
+     * \param app The application for which data should be removed.
+     * \param force Force deletion of the resource and all sub-resources.
+     * If false sub-resources will be kept if they are still referenced by
+     * resources that have been created by other applications.
+     */
+    void removeDataByApplication(const QString& app,
+                                 bool force);
+
+    /**
+     * Remove all statements involving any proerty from \p properties from
+     * all resources in \p resources if it was created by application \p app.
+     */
+    void removePropertiesByApplication(const QList<QUrl>& resources,
+                                       const QList<QUrl>& properties,
+                                       const QString& app);
+
+    /**
+     * \param resources The resources to be merged. Blank nodes will be converted into new
+     * URIs (unless the corresponding resource already exists).
+     * \param app The calling application
+     * \param additionalMetadata Additional metadata for the added resources. This can include
+     * such details as the creator of the data or details on the method of data recovery.
+     * One typical usecase is that the file indexer uses (rdf:type, nrl:DiscardableInstanceBase)
+     * to state that the provided information can be recreated at any time.
+     */
+    void mergeResources(const SimpleResourceGraph& resources,
+                        const QString& app,
+                        const QHash<QUrl, QVariant>& additionalMetadata);
+
+    /**
+     * Describe a set of resources, i.e. retrieve all their properties.
+     * \param resources The resource URIs of the resources to describe.
+     * \param includeSubResources If \p true sub resources will be included.
+     */
+    SimpleResourceGraph describeResources(const QList<QUrl>& resources,
+                                          bool includeSubResources);
+    //@}
+
+private:
+    class Private;
+    Private* const d;
+};
+
+#endif // DATAMANAGEMENTSERVICE_H
