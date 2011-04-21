49a219564e97d55d2e1cd81218027dcea7d131ef
Friedrich W. H. Kossebau <kossebau@kde.org>
2011-04-22 00:52:08 +0200

    Fixes: remove device if last service is removed in UpnpNetworkBuilder
    was shadowed by a useless local var

--- kioslave/network/network/builder/upnp/upnpnetworkbuilder.cpp
+++ kioslave/network/network/builder/upnp/upnpnetworkbuilder.cpp
@@ -249,7 +249,6 @@ void UpnpNetworkBuilder::removeUPnPDevices( const QList<Cagibi::Device>& upnpDev
                 // remove device on last service
                 if( d->serviceList().count() == 0 )
                 {
-                    QList<NetDevice> removedDevices;
                     removedDevices.append( device );
                     // remove only after taking copy from reference into removed list
                     it.remove();
