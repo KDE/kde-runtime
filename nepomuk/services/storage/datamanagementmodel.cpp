ceb3ce365d799bd265d97e8497a05db95bfa0f4d
Sebastian Trueg <trueg@kde.org>
2011-06-28 17:48:55 +0200

    Added unit tests for createResource.
    This lead to the discovery of a bug: the types parameter was not
    verified, ie. unknown types were allowed.

--- nepomuk/services/storage/datamanagementmodel.cpp
+++ nepomuk/services/storage/datamanagementmodel.cpp
@@ -736,10 +736,20 @@ QUrl Nepomuk::DataManagementModel::createResource(const QList<QUrl> &types, cons
         setError(QLatin1String("createResource: Empty application specified. This is not supported."), Soprano::Error::ErrorInvalidArgument);
         return QUrl();
     }
-    foreach(const QUrl& type, types) {
-        if(type.isEmpty()) {
-            setError(QLatin1String("createResource: Encountered empty type URI."), Soprano::Error::ErrorInvalidArgument);
-            return QUrl();
+    else if(types.isEmpty()) {
+        setError(QLatin1String("createResource: No type specified. Cannot create resources without a type."), Soprano::Error::ErrorInvalidArgument);
+        return QUrl();
+    }
+    else {
+        foreach(const QUrl& type, types) {
+            if(type.isEmpty()) {
+                setError(QLatin1String("createResource: Encountered empty type URI."), Soprano::Error::ErrorInvalidArgument);
+                return QUrl();
+            }
+            else if(!d->m_classAndPropertyTree->isKnownClass(type)) {
+                setError(QLatin1String("createResource: Encountered invalid type URI."), Soprano::Error::ErrorInvalidArgument);
+                return QUrl();
+            }
         }
     }
 
