1ba1a4294863bb96b139c0df448d19de2e83d9a2
Sebastian Trueg <trueg@kde.org>
2011-08-10 17:59:22 +0200

    Revert "Fixed compilation of unit test." It only applied to KDE/4.7
    This reverts commit 6d67fc9c85a339e5f91f367d3bbcb61cdf6c7a9a.

--- nepomuk/services/storage/test/datamanagementmodeltest.cpp
+++ nepomuk/services/storage/test/datamanagementmodeltest.cpp
@@ -2632,6 +2632,7 @@ void DataManagementModelTest::testRemoveDataByApplication_nieUrlRelated()
     QVERIFY(m_model->containsAnyStatement(res, QUrl("prop:/res"), fileResUri));
 
     QVERIFY(!haveTrailingGraphs());
+    QVERIFY(!haveDataInDefaultGraph());
 }
 
 // make sure the mtime is updated properly in different situations
