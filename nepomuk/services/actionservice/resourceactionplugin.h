eeadcfc45ea0f74501654aac78afcec049d4611e
Sebastian Trueg <trueg@kde.org>
2011-08-11 21:40:58 +0200

    Added service and plugin base class skeletons for the action service.

---
+++ nepomuk/services/actionservice/resourceactionplugin.h
@@ -0,0 +1,44 @@
+/*
+   This file is part of the Nepomuk KDE project.
+   Copyright (C) 2011 Sebastian Trueg <trueg@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) version 3, or any
+   later version accepted by the membership of KDE e.V. (or its
+   successor approved by the membership of KDE e.V.), which shall
+   act as a proxy defined in Section 6 of version 3 of the license.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef RESOURCEACTIONPLUGIN_H
+#define RESOURCEACTIONPLUGIN_H
+
+#include <QObject>
+
+#include "nepomukactions_export.h"
+
+namespace Nepomuk {
+
+class NEPOMUKACTIONS_EXPORT ResourceActionPlugin : public QObject
+{
+    Q_OBJECT
+
+public:
+    ResourceActionPlugin(QObject *parent = 0);
+
+public slots:
+
+};
+
+}
+
+#endif // RESOURCEACTIONPLUGIN_H
