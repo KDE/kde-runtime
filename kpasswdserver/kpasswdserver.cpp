KDE/kde-runtime

commit ac8db2eb9ba6798573eee08e904ca5b778d502f8

- Remove registration of KIO::AuthInfo's meta type. It is done when   KIO::AuthInfo is created.
- Revert commit 97105ca32bb35f8a470982ff53142c02e95f2009 and partially
  revert commit 2317acb1da0eefef322d9bac046187fc067cc27b since they
  break KPasswdServer's ability to properly show the retry dialog.
  Instead always cache the password in the wallet when addAuthInfo
  is called.

kpasswdserver/kpasswdserver.cpp
@@ -74,7 +74,6 @@ KPasswdServer::AuthInfoContainer::Sorter::operator ()(AuthInfoContainer* n1, Aut
 KPasswdServer::KPasswdServer(QObject* parent, const QList<QVariant>&)
  : KDEDModule(parent)
 {
-    KIO::AuthInfo::registerMetaTypes();
     m_seqNr = 0;
     m_wallet = 0;
 
@@ -417,10 +416,20 @@ void
 KPasswdServer::addAuthInfo(const KIO::AuthInfo &info, qlonglong windowId)
 {
     kDebug(debugArea()) << "User =" << info.username << ", RealmValue= " << info.realmValue
-                << ", WindowId = " << windowId << endl;
-    QString key = createCacheKey(info);
+                        << ", WindowId = " << windowId << endl;
+    const QString key = createCacheKey(info);
 
     m_seqNr++;
+
+    if (openWallet(windowId) && storeInWallet(m_wallet, key, info)) {
+        // Since storing the password in the wallet succeeded, make sure the
+        // password information is stored in memory only for the duration the
+        // windows associated with it are still around.
+        AuthInfo authToken (info);
+        authToken.keepPassword = false;
+        addAuthInfoItem(key, authToken, windowId, m_seqNr, false);
+        return;
+    }
 
     addAuthInfoItem(key, info, windowId, m_seqNr, false);
 }
@@ -626,7 +635,7 @@ KPasswdServer::processRequest()
         }
         if ( dlgResult != QDialog::Accepted )
         {
-            if (!bypassCacheAndKWallet && !skipAutoCaching && request->prompt)
+            if (!bypassCacheAndKWallet && request->prompt)
             {
                 addAuthInfoItem(request->key, info, 0, m_seqNr, true);
             }
@@ -634,7 +643,7 @@ KPasswdServer::processRequest()
         }
         else
         {
-            if (!bypassCacheAndKWallet && !skipAutoCaching)
+            if (!bypassCacheAndKWallet)
             {
                 addAuthInfoItem(request->key, info, request->windowId, m_seqNr, false);
             }
@@ -806,7 +815,7 @@ KPasswdServer::addAuthInfoItem(const QString &key, const KIO::AuthInfo &info, ql
    AuthInfoContainer *current = 0;
    Q_FOREACH(current, *authList)
    {
-       if (!info.realmValue.isEmpty() && (current->info.realmValue == info.realmValue))
+       if (current->info.realmValue == info.realmValue)
        {
           authList->removeOne(current);
           break;
@@ -829,12 +838,7 @@ KPasswdServer::addAuthInfoItem(const QString &key, const KIO::AuthInfo &info, ql
    current->seqNr = seqNr;
    current->isCanceled = canceled;
 
-   // Attempt to store the password into the wallet.
-   const bool storedPasswd = (openWallet(windowId) && storeInWallet(m_wallet, key, info));
-
-   // Unless storing the password information into the wallet above fails,
-   // expire the password stored in this KPasswdServer upon window close.
-   updateAuthExpire(key, current, windowId, (!storedPasswd && !canceled));
+   updateAuthExpire(key, current, windowId, (info.keepPassword && !canceled));
 
    // Insert into list, keep the list sorted "longest path" first.
    authList->append(current);

commit 5b1f8e350a987769c9f1e68b07ef45c0ec70b3cb

Disable use of KWallet from the KPasswdserver unit test.
Otherwise the unittest pops up kwallet dialogs, and thus isn't automatic anymore.
Commit ac8db2eb9 broke this.
CCMAIL: adawit@kde.org

kpasswdserver/kpasswdserver.cpp
@@ -77,6 +77,7 @@ KPasswdServer::KPasswdServer(QObject* parent, const QList<QVariant>&)
     KIO::AuthInfo::registerMetaTypes();
     m_seqNr = 0;
     m_wallet = 0;
+    m_walletDisabled = false;
 
     KPasswdServerAdaptor *adaptor = new KPasswdServerAdaptor(this);
     // register separately from kded
@@ -422,7 +423,7 @@ KPasswdServer::addAuthInfo(const KIO::AuthInfo &info, qlonglong windowId)
 
     m_seqNr++;
 
-    if (openWallet(windowId) && storeInWallet(m_wallet, key, info)) {
+    if (!m_walletDisabled && openWallet(windowId) && storeInWallet(m_wallet, key, info)) {
         // Since storing the password in the wallet succeeded, make sure the
         // password information is stored in memory only for the duration the
         // windows associated with it are still around.

kpasswdserver/kpasswdserver.h
@@ -45,6 +45,9 @@ public:
   KPasswdServer(QObject* parent, const QList<QVariant>& = QList<QVariant>());
   ~KPasswdServer();
 
+  // Called by the unit test
+  void setWalletDisabled(bool d) { m_walletDisabled = d; }
+
 public Q_SLOTS:
   qlonglong checkAuthInfoAsync(KIO::AuthInfo, qlonglong, qlonglong);
   qlonglong queryAuthInfoAsync(const KIO::AuthInfo &, const QString &, qlonglong, qlonglong, qlonglong);
@@ -116,6 +119,7 @@ protected:
   QHash<int, QStringList> mWindowIdList;
   QStringList m_authPrompted;
   KWallet::Wallet* m_wallet;
+  bool m_walletDisabled;
   qlonglong m_seqNr;
 };
 

kpasswdserver/tests/kpasswdservertest.cpp
@@ -38,6 +38,7 @@ private Q_SLOTS:
     void simpleTest()
     {
         KPasswdServer server(this);
+        server.setWalletDisabled(true);
 
         // Check that processRequest doesn't crash when it has nothing to do
         server.processRequest();
@@ -75,6 +76,7 @@ private Q_SLOTS:
     void testCheckDuringQuery()
     {
         KPasswdServer server(this);
+        server.setWalletDisabled(true);
         KIO::AuthInfo info;
         info.url = KUrl("http://www.kde.org");
 
@@ -111,6 +113,7 @@ private Q_SLOTS:
     void testExpiry()
     {
         KPasswdServer server(this);
+        server.setWalletDisabled(true);
         KIO::AuthInfo info;
         info.url = KUrl("http://www.example.com");
 
@@ -137,6 +140,7 @@ private Q_SLOTS:
     void testFillDialog()
     {
         KPasswdServer server(this);
+        server.setWalletDisabled(true);
         // What the app would ask
         KIO::AuthInfo info;
         info.url = KUrl("http://www.example.com");