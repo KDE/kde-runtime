249b556a3bd0af36b5a26deed24f2f3ce84d5261
Albert Astals Cid <aacid@kde.org>
2012-09-06 20:10:20 +0200

    This moved to kdelibs

--- kcontrol/locale/Messages.sh
+++ kcontrol/locale/Messages.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC *.ui >> rc.cpp
 $XGETTEXT -ktranslate:1,1t -ktranslate:1c,2,2t *.cpp -o $podir/kcmlocale.pot
-$XGETTEXT TIMEZONES -o $podir/../kdelibs/timezones4.pot
