86dc8cdda1b368c11abdb22ad9a5d729e60fb322
Sebastian Kügler <sebas@kde.org>
2012-10-26 02:36:32 +0200

    remove position, mouse and size handling

--- plasma/scriptengines/javascript/declarative/internaltoolbox.cpp
+++ plasma/scriptengines/javascript/declarative/internaltoolbox.cpp
@@ -88,34 +88,6 @@ Plasma::Containment *InternalToolBox::containment()
     return m_containment;
 }
 
-QPoint InternalToolBox::toolPosition(int toolHeight)
-{
-    switch (corner()) {
-    case TopRight:
-        return QPoint(boundingRect().width(), -toolHeight);
-    case Top:
-        return QPoint((int)boundingRect().center().x() - boundingRect().width(), -toolHeight);
-    case TopLeft:
-        return QPoint(-boundingRect().width(), -toolHeight);
-    case Left:
-        return QPoint(-boundingRect().width(), (int)boundingRect().center().y() - boundingRect().height());
-    case Right:
-        return QPoint(boundingRect().width(), (int)boundingRect().center().y() - boundingRect().height());
-    case BottomLeft:
-        return QPoint(-boundingRect().width(), toolHeight);
-    case Bottom:
-        return QPoint((int)boundingRect().center().x() - m_iconSize.width(), toolHeight);
-    case BottomRight:
-    default:
-        return QPoint(boundingRect().width(), toolHeight);
-    }
-}
-
-QGraphicsWidget *InternalToolBox::toolParent()
-{
-    return this;
-}
-
 QList<QAction *> InternalToolBox::actions() const
 {
     return m_actions;
@@ -191,17 +163,6 @@ void InternalToolBox::setShowing(const bool show)
     m_showing = show;
 }
 
-void InternalToolBox::mousePressEvent(QGraphicsSceneMouseEvent *event)
-{
-    if (event->button() == Qt::LeftButton) {
-        event->accept();
-        // set grab position relative to toolbox
-        m_dragStartRelative = mapToParent(event->pos()).toPoint() - pos().toPoint();
-    } else {
-        event->ignore();
-    }
-}
-
 QSize InternalToolBox::cornerSize() const
 {
     return boundingRect().size().toSize();
@@ -217,117 +178,6 @@ QSize  InternalToolBox::fullHeight() const
     return boundingRect().size().toSize();
 }
 
-void InternalToolBox::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
-{
-    if (!m_movable || (!m_dragging && boundingRect().contains(event->pos()))) {
-        return;
-    }
-
-    m_dragging = true;
-    m_userMoved = true;
-    const QPoint newPos = mapToParent(event->pos()).toPoint();
-    const QPoint curPos = pos().toPoint();
-
-    const QSize cSize = cornerSize();
-    const QSize fHeight = fullHeight();
-    const QSize fWidth = fullWidth();
-    const int h = fHeight.height();
-    const int w = fWidth.width();
-
-    const int areaWidth = parentWidget()->size().width();
-    const int areaHeight = parentWidget()->size().height();
-
-    int x = curPos.x();
-    int y = curPos.y();
-
-    // jump to the nearest desktop border
-    int distanceToLeft = newPos.x() - m_dragStartRelative.x();
-    int distanceToRight = areaWidth - w - distanceToLeft;
-    int distanceToTop = newPos.y() - m_dragStartRelative.y();
-    int distanceToBottom = areaHeight - h - distanceToTop;
-
-    int distancetoHorizontalMiddle = qAbs((newPos.x() + boundingRect().size().width()/2) - areaWidth/2 - m_dragStartRelative.x());
-    int distancetoVerticalMiddle = qAbs((newPos.y() + boundingRect().size().height()/2) - areaHeight/2 - m_dragStartRelative.y());
-
-    if (distancetoHorizontalMiddle < 10) {
-        x = areaWidth/2 - boundingRect().size().width()/2;
-    } else if (distancetoVerticalMiddle < 10) {
-        y = areaHeight/2 - boundingRect().size().height()/2;
-    } else {
-        // decide which border is the nearest
-        if (distanceToLeft < distanceToTop && distanceToLeft < distanceToRight &&
-            distanceToLeft < distanceToBottom ) {
-            x = 0;
-            y = (newPos.y() - m_dragStartRelative.y());
-        } else if (distanceToRight < distanceToTop && distanceToRight < distanceToLeft &&
-                distanceToRight < distanceToBottom) {
-            x = areaWidth - w;
-            y = (newPos.y() - m_dragStartRelative.y());
-        } else if (distanceToTop < distanceToLeft && distanceToTop < distanceToRight &&
-                distanceToTop < distanceToBottom ) {
-            y = 0;
-            x = (newPos.x() - m_dragStartRelative.x());
-        } else if (distanceToBottom < distanceToLeft && distanceToBottom < distanceToRight &&
-                distanceToBottom < distanceToTop) {
-            y = areaHeight - h;
-            x = (newPos.x() - m_dragStartRelative.x());
-        }
-    }
-
-
-    x = qBound(0, x, areaWidth - w);
-    y = qBound(0, y, areaHeight - h);
-
-    Corner newCorner = corner();
-    if (x == 0) {
-        if (y == 0) {
-            newCorner = TopLeft;
-        } else if (areaHeight - cSize.height() < newPos.y()) {
-            y = areaHeight - cSize.height();
-            newCorner = BottomLeft;
-        } else {
-            newCorner = Left;
-        }
-    } else if (y == 0) {
-        if (areaWidth - cSize.width() < newPos.x()) {
-            x = areaWidth - cSize.width();
-            newCorner = TopRight;
-        } else {
-            newCorner = Top;
-        }
-    } else if (x + w >= areaWidth) {
-        if (areaHeight - cSize.height() < newPos.y()) {
-            y = areaHeight - cSize.height();
-            x = areaWidth - cSize.width();
-            newCorner = BottomRight;
-        } else {
-            x = areaWidth - fHeight.width();
-            newCorner = Right;
-        }
-    } else {
-        y = areaHeight - fWidth.height();
-        newCorner = Bottom;
-    }
-
-    if (newCorner != corner()) {
-        prepareGeometryChange();
-        setCorner(newCorner);
-    }
-
-    setPos(x, y);
-}
-
-void InternalToolBox::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
-{
-    if (event->button() == Qt::LeftButton && !m_dragging && boundingRect().contains(event->pos())) {
-        emit toggled();
-        return;
-    }
-
-    m_dragging = false;
-    KConfigGroup cg(m_containment->config());
-    save(cg);
-}
 
 bool InternalToolBox::isMovable() const
 {
@@ -430,88 +280,6 @@ void InternalToolBox::immutabilityChanged(Plasma::ImmutabilityType immutability)
         setVisible(unlocked);
     } else {
         setIsMovable(unlocked);
-    }
-}
-
-void InternalToolBox::reposition()
-{
-    updateToolBox();
-
-    if (m_userMoved) {
-        restore(m_containment->config());
-        return;
-    }
-
-    if (m_containment->containmentType() == Plasma::Containment::PanelContainment ||
-        m_containment->containmentType() == Plasma::Containment::CustomPanelContainment) {
-        QRectF rect = boundingRect();
-        if (m_containment->formFactor() == Plasma::Vertical) {
-            setCorner(InternalToolBox::Bottom);
-            setPos(m_containment->geometry().width() / 2 - rect.width() / 2,
-                   m_containment->geometry().height() - rect.height());
-        } else {
-            //defaulting to Horizontal right now
-            if (QApplication::layoutDirection() == Qt::RightToLeft) {
-                setPos(m_containment->geometry().left(),
-                       m_containment->geometry().height() / 2 - rect.height() / 2);
-                setCorner(InternalToolBox::Left);
-            } else {
-                setPos(m_containment->geometry().width() - rect.width(),
-                       m_containment->geometry().height() / 2 - rect.height() / 2);
-                setCorner(InternalToolBox::Right);
-            }
-        }
-
-        //kDebug() << "got ourselves a panel containment, moving to" << pos();
-    } else if (m_containment->corona()) {
-        //kDebug() << "desktop";
-
-        int screen = m_containment->screen();
-        QRectF avail = m_containment->geometry();
-        QRectF screenGeom = avail;
-
-        if (screen > -1 && screen < m_containment->corona()->numScreens()) {
-            avail = m_containment->corona()->availableScreenRegion(screen).boundingRect();
-            screenGeom = m_containment->corona()->screenGeometry(screen);
-            avail.translate(-screenGeom.topLeft());
-        }
-
-        // Transform to the containment's coordinate system.
-        screenGeom.moveTo(0, 0);
-
-        if (!m_containment->view() || !m_containment->view()->transform().isScaling()) {
-            if (QApplication::layoutDirection() == Qt::RightToLeft) {
-                if (avail.top() > screenGeom.top()) {
-                    setPos(avail.topLeft() - QPoint(0, avail.top()));
-                    setCorner(InternalToolBox::Left);
-                } else if (avail.left() > screenGeom.left()) {
-                    setPos(avail.topLeft() - QPoint(boundingRect().width(), 0));
-                    setCorner(InternalToolBox::Top);
-                } else {
-                    setPos(avail.topLeft());
-                    setCorner(InternalToolBox::Top);
-                }
-            } else {
-                if (avail.top() > screenGeom.top()) {
-                    setPos(avail.topRight() - QPoint(boundingRect().width(), -avail.top()));
-                    setCorner(InternalToolBox::Right);
-                } else if (avail.right() < screenGeom.right()) {
-                    setPos(avail.topRight() - QPoint(boundingRect().width(), 0));
-                    setCorner(InternalToolBox::Top);
-                } else {
-                    setPos(avail.topRight() - QPoint(boundingRect().width(), 0));
-                    setCorner(InternalToolBox::Top);
-                }
-            }
-        } else {
-            if (QApplication::layoutDirection() == Qt::RightToLeft) {
-                setPos(m_containment->mapFromScene(QPointF(m_containment->geometry().topLeft())));
-                setCorner(InternalToolBox::Top);
-            } else {
-                setPos(m_containment->mapFromScene(QPointF(m_containment->geometry().topRight())));
-                setCorner(InternalToolBox::Top);
-            }
-        }
     }
 }
 
