a8385b3bc8e1494262232221384e55492b5f2b95
Aleix Pol <aleixpol@kde.org>
2014-03-07 16:26:27 +0100

    Cleanup root CMakeLists.txt file
    Moves checks into the directory that requires it.
    Removes unneeded things.
    Removes a kde4 script that didn't do anything.

--- config-runtime.h.cmake
+++ config-runtime.h.cmake
@@ -24,9 +24,6 @@
 /* Define to 1 if you have the <sys/socket.h> header file. */
 #cmakedefine HAVE_SYS_SOCKET_H 1
 
-/* Define to 1 if you have the <sys/time.h> header file. */
-#cmakedefine HAVE_SYS_TIME_H 1
-
 /* Define to 1 if you have the <sys/types.h> header file. */
 #cmakedefine HAVE_SYS_TYPES_H 1
 
